5259a8f8f3d6045602ab4460d5590f2c83afc4ba
Jatin Yadav <139036091+Jatin-11013@users.noreply.github.com>
2026-03-03 18:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.caption("Operation Team – Safe vs Loss Booking Tool")
 
 # ---------------- DI MASTER ----------------
 supplier_di = {
-    "TBO Flights Online - BOMA774": 0.0064,
+    "TBO Flights Online - BOMA774": 0,
     "FlyShop Series Online API": 0.01,
     "Flyshop online API": 0.01,
     "Cleartrip Private Limited - AB 2": 0.01,
@@ -523,6 +523,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 
 
 
